feabe3fbabb90d12b073a1b6c8fa34fbdbf16b8e
Tharinsa Makaranda <tharinsamakaranda22@gmail.com>
2026-08-30 15:59:39 +0530

    Fix PetPanel constructor and improve layout

--- src/view/PetPanel.java
+++ src/view/PetPanel.java
@@ -1,8 +1,60 @@
+
 package view;
 
-import javax.swing.*;
-import java.awt;
+import dao.OwnerDAO;
+import dao.PetDAO;
+import model.Owner;
 import model.Pet;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt;
+import java.util.List;
+
+public class PetPanel extends JPanel
+{
+	private final PetDAO petDAO = new PetDAO();
+	private final OwnerDAO petDAO = new PetDAO();
+	private DafaultTableModel model;
+	private JTable table;
+
+	public Petpanel()
+	{
+		setlayout(new BorderLayout(10, 10));
+		setBackground(UIStyle.LIGHT_BG);
+		setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
+
+		JPanel top = new JPanel (new BorderLayout());
+		top.setOpaque(false);
+		JLabel title = new JLabel("Pet Management");
+		title.setFont(UIStyle.FONT_TITLE);
+		top.add(title, BorderLayout.WEST);
+
+		RoundButton addBtn = new RoundButoon("+ Add New Pet", UIStyle.PINK_ACCENT, Color.WHITE);
+		addBtn.setPreferredSize(new Dimension(160, 36));
+		addBtn.addActionListener(e -> showForm(null));
+		top.add(addBtn, BorderLayout.EAST);
+		add(top, BorderLayout.NORTH);
+
+		String[] cols = {"Pet ID", "Name", "Species", "Breed", "Age", "Gender", "Owner ID"};
+		model = new DefaultTableModel(cols, 0){
+			public boolean isCellEditable(int r, int C){ return false; }
+		};
+		table = new JTable(Model);
+		table.setRowHeight(28);
+		add(new JScrollPane(table), BorderLayout.CENTER);
+
+		JPanel bottom = new JPanel(new FlowLayout (FlowLayout.RIGHT));
+		bottom.setOpaque(false);
+		JButton editBtn = new JButton("Edit Selected");
+        JButton delBtn = new JButton("Delete Selected");
+        editBtn.addActionListener(e -> editSelected());
+        delBtn.addActionListener(e -> deleteSelected());
+        bottom.add(editBtn);
+        bottom.add(delBtn);
+        add(bottom, BorderLayout.SOUTH);
+	}
+}
 
 public class PetsGUI extends BaseFormGUI
 {

95cbec58cd99102a7f5a4470b56ccce17a2bba95
Druwinka Fernando <druwinkaf@gmail.com>
2026-08-30 19:17:38 +0530

    Add UIStyle.java file

---
+++ src/view/UIStyle.java
@@ -0,0 +1,23 @@
+package view;
+
+import java.awt.*;
+
+//centeral place for the "PetBliss" color palett and font,
+
+public class UIStyle {
+	//color
+	public static final Color SIDEBAR_BG       = new Color(23, 48, 49);
+	public static final Color PINK_ACCENT      = new Color(232, 104, 131);
+	public static final Color LIGHT_BG         = new Color(255, 246, 247);
+	public static final Color CARD_PINK        = new Color(253, 225, 231);
+	public static final Color CARD_BLUE        = new Color(223, 236, 255);
+	public static final Color CARD_PURPLE      = new Color(232, 222, 250);
+	public static final Color FORM_GREEN       = new Color(216, 245, 226);
+	
+	//font
+	public static final Font FONT_TITLE   = new Font("SanaSerif", Font.BOLD, 24);
+	public static final Font FONT_HEADER  = new Font("SanaSerif", Font.BOLD, 16);
+	public static final Font FONT_NORMAL   = new Font("SanaSerif", Font.PLAIN, 14);
+	public static final Font FONT_BOLD   = new Font("SanaSerif", Font.BOLD, 14);
+	
+}
